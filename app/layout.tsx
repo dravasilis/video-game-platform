9e47d8d59d5abecbcb4c1d83cee757e382df9153
dravasilis <dravasilis@gmail.com>
2025-03-14 11:34:52 +0200

    feat: add favicon and app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import Head from "next/head";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "Video Game Platform", // Base Title
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+      {" "}
+      <Head>
+        <title>My page title</title>
+      </Head>
       <Link href="/home">Home</Link>
     </>
   );
