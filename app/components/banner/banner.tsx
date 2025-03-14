258484f38ab78143ae6652460270213b60c6ecce
dravasilis <dravasilis@gmail.com>
2025-03-14 16:16:58 +0200

    feat: create landing page and banner

---
+++ app/components/banner/banner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./banner.css";
+const Banner = () => {
+  return (
+    <div>
+      <div className="bannerGradient banner"></div>
+      <img className="banner" src="/images/banner.jpg"></img>
+    </div>
+  );
+};
+
+export default Banner;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import Head from "next/head";
 import Link from "next/link";
+import Banner from "./components/banner/banner";
 
 export default function Home() {
   return (
     <div>
-      <Link href="/home">Home</Link>
+      <Banner />
+      <div className="z-10 relative top-[15rem] px-32 flex flex-col gap-4">
+        <div className="flex gap-2 items-center">
+          <img src="favicon.ico" width={50} alt="" />
+          <span className="text-5xl ">Video Game Platform</span>
+        </div>
+        <span className="text-2xl text-[#b1c3d6]">
+          Explore, discover, save your favourite games{" "}
+        </span>
+        <div className="flex w-full justify-end">
+          <Link href={"/home"} className="underlineEffect flex gap-2">
+            <span>Start browsing</span>
+            <img src="/svg/next.svg" width={20} alt="" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
