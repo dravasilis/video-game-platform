755592533df73963456b4b658e419627e93d79c9
dravasilis <103889224+dravasilis@users.noreply.github.com>
2025-03-16 16:58:07 +0200

    fix: banner responsiveness

--- app/components/banner/banner.tsx
+++ app/components/banner/banner.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import "./banner.css";
+import  "./banner.css";
 const Banner = () => {
-	return (
-		<>
-			<div className="banner"></div>
-		</>
-	);
+  return (
+    <div>
+      <div className="bannerGradient banner"></div>
+      <img className="banner   object-fill" src="/images/bg1.jpg"></img>
+    </div>
+  );
 };
 
 export default Banner;
