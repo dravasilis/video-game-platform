beef539677fd0495caf6d4771cc7e24b244c72a4
dravasilis <dravasilis@gmail.com>
2025-03-18 11:29:59 +0200

    config: change api key to vercel env variable

--- app/helpers/fetch-helper.ts
+++ app/helpers/fetch-helper.ts
@@ -2,10 +2,10 @@ import { environment } from "@/env/environment";
 import { BasicPagination } from "../models/pagination";
 
 export const fetchHelper = async <T extends BasicPagination>(endpoint: string, pagination: T = { page_size: 100 } as T) => {
-    // const API_KEY = process.env.API_KEY;
+    const API_KEY = process.env.API_KEY;
     const params = new URLSearchParams({
-        // key: API_KEY ?? '',
-        key: environment.RAWGApiKey,
+        key: API_KEY ?? '',
+        // key: environment.RAWGApiKey,
         ...Object.fromEntries(Object.entries(pagination).map(([key, value]) => [key, String(value)])) // Ensure values are strings
     });
     const res = await fetch(
