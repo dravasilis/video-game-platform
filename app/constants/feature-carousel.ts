0a3405c8e94a5a601754be251bd0574f8657f429
dravasilis <dravasilis@gmail.com>
2025-03-19 11:31:33 +0200

    feat: create constants for features objects

---
+++ app/constants/feature-carousel.ts
@@ -0,0 +1,18 @@
+import { FeatureCarousel } from "../models/feature-showcase";
+
+export const CarouselFeatures: FeatureCarousel[] = [
+    {
+        title: 'Games Releasing Soon',
+        description: "Stay ahead of the game with our upcoming releases! Explore highly anticipated titles, get early insights and be the first to experience the next big adventures.",
+    },
+    {
+        title: 'Old Gems',
+        description: "Rediscover the classics that defined gaming! From timeless adventures to legendary titles, explore the best games that continue to captivate players across generations.",
+    },
+    {
+        title: 'Top Rated Games',
+        description: "Explore the highest-rated games loved by players and critics alike! From groundbreaking adventures to must-play masterpieces, discover the best of the best in gaming.",
+    },
+
+
+];
