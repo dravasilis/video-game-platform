f322f105d7b66c855c5a7731994eb4e6834f4c6d
dravasilis <dravasilis@gmail.com>
2025-03-19 11:31:41 +0200

    style: style fixes

--- app/components/feature-showcase/feature-showcase.tsx
+++ app/components/feature-showcase/feature-showcase.tsx
@@ -13,9 +13,9 @@ const FeatureShowcase = ({ title, description, image }: Props) => {
   return (
     <div className="relative flex flex-col w-full h-max">
       <div className="flex flex-col gap-4 col-span-2 z-20">
-        <span className="text-5xl max-lg:text-4xl max-md:text-3xl  text-shadow text-primary-100  ">
+        <h3 className="text-5xl max-lg:text-4xl max-md:text-3xl  text-shadow text-primary-100  ">
           {title}
-        </span>
+        </h3>
         <div className="w-full h-1 rounded-lg bg-primary-100 box-shadow"></div>
       </div>
       <Image
--- app/components/landing-page/landing-page.tsx
+++ app/components/landing-page/landing-page.tsx
@@ -10,12 +10,15 @@ import FeatureShowcase from "../feature-showcase/feature-showcase";
 import StatCard from "../stat-card/stat";
 import logo from "../../../public/images/logo.png";
 import nextSvg from "../../../public/svg/next.svg";
+import { FeatureShowcases } from "@/app/constants/feature-showcases";
+import { CarouselFeatures } from "@/app/constants/feature-carousel";
 
 const LandingPage = async () => {
   const currentDate = new Date().toISOString().split("T")[0];
   //   console.log(currentDate);
-
-  const [upcomingGamesRes, vintageGamesRes, mostRatedGamesRes]: [
+  const featureShowcases = FeatureShowcases;
+  const carouselFeatures = CarouselFeatures;
+  const [upcomingGamesRes, vintageGamesRes, topRatedGamesRes]: [
     HttpResponse<Game>,
     HttpResponse<Game>,
     HttpResponse<Game>
@@ -28,13 +31,16 @@ const LandingPage = async () => {
       ordering: "ratings_count",
       dates: "1995-01-01,2005-01-01",
     }),
-    fetchHelper("/games", { ordering: "-metacritic" }),
+    fetchHelper("/games", {
+      ordering: "-metacritic",
+      dates: "2012-01-01,2025-01-01",
+    }),
   ]);
   //   console.log([mostRatedGamesRes]);
   return (
-    <div>
+    <div className="">
       <Banner />
-      <div className="z-10 relative top-[15rem] max-lg:top-[7rem] max-[600px]:!top-[5rem] px-32 max-xl:px-24 max-lg:px-20 max-md:px-12 max-sm:px-5 flex flex-col gap-4">
+      <div className="z-10 animate-fade-in relative top-[15rem] max-lg:top-[7rem] max-[600px]:!top-[5rem] px-32 max-xl:px-24 max-lg:px-20 max-md:px-12 max-sm:px-5 flex flex-col gap-4">
         {/* TITLE  */}
         <div className="flex gap-4 items-center">
           <Image
@@ -42,7 +48,7 @@ const LandingPage = async () => {
             alt="favicon"
             className="w-[50px] max-sm:w-[40px]"
           />
-          <h1 className="text-5xl max-sm:text-4xl font-bold flex tracking-[-2px]">
+          <h1 className=" text-5xl max-sm:text-4xl font-bold flex tracking-[-2px]">
             Gamepedia
           </h1>
         </div>
@@ -57,7 +63,7 @@ const LandingPage = async () => {
             svg="game"
             title="Games"
             count={
-              (mostRatedGamesRes.count ?? 0).toString().substring(0, 3) + "K"
+              (topRatedGamesRes.count ?? 0).toString().substring(0, 3) + "K"
             }
           />
           <StatCard svg="star" title="Reviews" count="500K" />
@@ -67,15 +73,13 @@ const LandingPage = async () => {
         <div className="flex w-full justify-end">
           <Link href={"/"} className="underlineEffect flex gap-2">
             <span className="text-shadow">Start browsing</span>
-            <Image src={nextSvg} width={30} height={30} alt="next" />
+            <Image src={nextSvg} width={20} height={30} alt="next" />
           </Link>
         </div>
         {/* DESCRIPTION  */}
-        <div className="flex flex-col gap-4 py-20 max-md:pt-12">
+        <div className="flex flex-col gap-4 py-20 max-md:pt-12 max-sm:pt-4">
           <div className="flex items-center gap-2 text-3xl ">
-            <span className="font-bold">
-              All Your Favorite Games in One Place
-            </span>
+            <h3 className="font-bold">All Your Favorite Games in One Place</h3>
           </div>
           <span className="text-xl text-start max-lg:text-lg !w-3/4 max-xl:!w-full text-primary-150 leading-[30px]  max-sm:leading-6">
             Explore the ultimate game platform where you can discover, track,
@@ -87,50 +91,38 @@ const LandingPage = async () => {
             place.
           </span>
         </div>
-        <FeatureShowcase
-          title="Explore A Vast Library Of Games"
-          image="bg6.png"
-          description=" Dive into an extensive collection of games spanning all genres,
-              from action-packed adventures to immersive RPGs. Whether you're
-              searching for the latest releases or nostalgic classics, our
-              platform lets you explore, discover, and find your next favorite
-              game with ease."
-        />
-        <FeatureShowcase
-          title="Discover New Gaming Adventures"
-          image="bg3.png"
-          description=" Uncover new worlds, connect with unforgettable characters, and
-              dive into fresh gaming experiences like never before. Whether
-              you're exploring hidden gems or the latest blockbusters, every
-              game brings a unique adventure waiting to be discovered."
-        />
-        <FeatureShowcase
-          title="Discover Games Through Player Perspectives"
-          image="bg7.png"
-          description="Make informed gaming choices with real user reviews and ratings. Explore what players love (or don’t) about each game, share your own experiences, and connect with a community that values honest opinions. Whether you're searching for a hidden gem or the next big hit, let reviews guide your next adventure! "
-        />
-        <FeatureShowcase
-          title="Create & Customize Your Ultimate Game List"
-          image="bg5.png"
-          description="Easily save and organize your favorite games in one place! Build your personalized game list, keep track of the titles you love, and revisit them anytime. Whether you're curating a must-play backlog or showcasing your top picks, your collection is always just a click away. "
-        />
-        <div className="flex flex-col gap-12">
-          <FeatureShowcaseCarousel
-            description="Stay ahead of the game with our upcoming releases! Explore highly anticipated titles, get early insights,
-           and be the first to experience the next big adventures."
-            games={upcomingGamesRes.results}
-            title="Games Releasing Soon"
+        {featureShowcases.map((feature, index) => (
+          <FeatureShowcase
+            key={index}
+            title={feature.title}
+            image={feature.image}
+            description={feature.description}
           />
+        ))}
+        {CarouselFeatures.map((feature, index) => (
           <FeatureShowcaseCarousel
-            description="Rediscover the classics that defined gaming! From timeless adventures to legendary titles, explore the best games that continue to captivate players across generations."
-            games={vintageGamesRes.results}
-            title="Old Gems"
+            key={index}
+            description={feature.description}
+            games={
+              index === 0
+                ? upcomingGamesRes.results
+                : index === 1
+                ? vintageGamesRes.results
+                : topRatedGamesRes.results
+            }
+            title={feature.title}
           />
-          <FeatureShowcaseCarousel
-            description="Explore the highest-rated games loved by players and critics alike! From groundbreaking adventures to must-play masterpieces, discover the best of the best in gaming."
-            games={mostRatedGamesRes.results}
-            title="Top Rated Games"
-          />
+        ))}
+        <div className="flex items-center gap-1 py-8 justify-end">
+          Credit to
+          <Link
+            className="text-shadow underlineEffect text-[#3bffff]"
+            href="https://api.rawg.io/docs/"
+            target="_blank"
+          >
+            RAWG
+          </Link>
+          for providing the API
         </div>
       </div>
     </div>
