448b0de5ad850b2197b9fb236ec0106488a3caf3
dravasilis <103889224+dravasilis@users.noreply.github.com>
2025-03-23 14:42:43 +0200

    feat: add bg images and main nav reusable headers

--- app/components/landing-page/banner/banner.tsx
+++ app/components/landing-page/banner/banner.tsx
@@ -3,17 +3,19 @@ import "./banner.css";
 import Image from "next/image";
 interface Props {
   banner: string;
+  customBrightness?: boolean;
 }
-const Banner = ({ banner }: Props) => {
+const Banner = ({ banner,customBrightness }: Props) => {
   return (
     <div className="z-0">
       <div className="bannerGradient banner"></div>
       <Image
-        className="banner object-cover"
+       className={`banner object-cover ${customBrightness ? 'brightness-[.25]' : ''}`}
         width={1920}
         height={1080}
         src={`/images/${banner}`}
         alt="banner"
+        
       />
     </div>
   );
--- app/components/main-nav/main-nav.tsx
+++ app/components/main-nav/main-nav.tsx
@@ -1,25 +1,46 @@
 import Image from "next/image";
 import React from "react";
 import logo from "../../../public/images/logo.png";
-const MainNav = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <div className="flex flex-col gap-4 py-4 animate-fade-in">
-        {/* <Sidenav /> */}
-        <div className="flex gap-1 items-center z-10">
-          <Image
-            src={logo}
-            alt="favicon"
-            className="w-[30px] max-sm:w-[17px] mb-1"
-          />
-          <h1 className=" text-2xl text-primary-150 max-sm:text-sm font-bold flex tracking-[-2px]">
-            Gamepedia
-          </h1>
-        </div>
-        {children}
-      </div>
-    </>
-  );
+import Search from "../../search/search";
+
+interface Props {
+	children: React.ReactNode;
+	header?:string,
+	results?:number
+}
+const MainNav = ({ children,header,results }: Props) => {
+	return (
+		<>
+			<div className="flex flex-col gap-4 py-4 animate-fade-in px-4">
+				{/* <Sidenav /> */}
+				<div className="flex w-full justify-between  z-10">
+					<div className="flex gap-1 items-center z-10">
+						<Image
+							src={logo}
+							alt="favicon"
+							className="w-[30px] max-sm:w-[17px] mb-1"
+						/>
+						<h1 className=" text-2xl text-primary-150 max-sm:text-sm font-bold flex tracking-[-2px]">
+							Gamepedia
+						</h1>
+					</div>
+					<Search />
+				</div>
+				{header && results &&
+				<div className="flex justify-between w-full items-center z-10 px-20 max-lg:px-10 max-md:px-4 pt-8">
+						<h1 className="text-5xl max-sm:text-3xl text-primary-100 font-bold  ">
+						{header}
+						</h1>
+						<h3 className="text-primary-250 max-sm:text-xs">
+							{results.toLocaleString()} results
+						</h3>
+					</div>
+					 }
+				
+				{children}
+			</div>
+		</>
+	);
 };
 
 export default MainNav;
