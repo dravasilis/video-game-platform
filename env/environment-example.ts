e0d202344e7a73408c4aae36cd80f56379408b32
dravasilis <dravasilis@gmail.com>
2025-06-06 16:37:00 +0300

    fix: put firebase config into environment

--- env/environment-example.ts
+++ env/environment-example.ts
@@ -1,3 +1,12 @@
 export const environment = {
     RAWGApiKey: '' //api key goes here
 };
+export const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,6 @@
+import { firebaseConfig } from "@/env/environment";
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-const firebaseConfig = {
-    apiKey: "AIzaSyAMMzA0dN2-tNwUwCNnXzr-7m8o72bB4As",
-    authDomain: "video-game-platform.firebaseapp.com",
-    projectId: "video-game-platform",
-    storageBucket: "video-game-platform.firebasestorage.app",
-    messagingSenderId: "334334901567",
-    appId: "1:334334901567:web:9c92cca8ffa4cf3ee2678a",
-    measurementId: "G-RJNS6DCLN1"
-};
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
