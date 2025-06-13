e0498baed5f2615fd6469bd7405f81cf28185eb2
dravasilis <dravasilis@gmail.com>
2025-06-13 13:58:44 +0300

    fix: set login overlay to fixed position instead of absolute

--- app/components/auth-ui/login-modal/login-modal.tsx
+++ app/components/auth-ui/login-modal/login-modal.tsx
@@ -73,7 +73,7 @@ const LoginPopup = ({ onClose }: Props) => {
     <div
       id="overlay"
       onClick={() => onClose()}
-      className="absolute top-0 left-0 w-full h-full bg-black/60 flex items-center justify-center z-50"
+      className="fixed top-0 left-0 w-full h-full bg-black/60 flex items-center justify-center z-50"
     >
       <div
         onClick={(e) => e.stopPropagation()}
